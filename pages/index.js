e73bd15b6678a5847aef47d350d17aa353f5623e
Felipe Malheiros Barros <felipemalheirosb@gmail.com>
2025-11-08 18:57:01 +0000

    audio

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 function Home() {
-    return <h1>Zefinha, se você ama da uma risadinha!</h1>
+  const [audio] = useState(new Audio("/som.mp3"));
+
+  const tocarSom = () => {
+    audio.play();
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Mariana, clique aqui!</h1>
+      <button onClick={tocarSom}>Clique aqui 🎵</button>
+    </div>
+  );
 }
 
 export default Home;
